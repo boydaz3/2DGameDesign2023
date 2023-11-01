111758523e87c64f50afc66ac017048aebcea43b
konekowo <70107937+konekowo@users.noreply.github.com>
2023-11-01 11:55:39 -0400

    camera

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,4 @@
+// Suvan Mangamuri
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,24 +7,28 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    // these variables are set in the Unity Editor
     public ParticleSystem particleSystem;
     public ParticleSystem particleSystemJump;
+    
     private ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
     private bool doEmitParticles = false;
     
-    public float speed = 6f;
-    public float jumpSpeed = 5f;
+    public float speed = 7f;
+    public float jumpSpeed = 6.5f;
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        // make sure particle systems arent playing at the start
         particleSystem.Stop();
         particleSystemJump.Stop();
+
+
         
         InvokeRepeating("emitParticles", 0, 0.1f);
-        
     }
 
-
+    // every 100 ms emit particles if player is moving
     private void emitParticles()
     {
         if (doEmitParticles)
@@ -35,16 +40,21 @@ public class Movement : MonoBehaviour
     
     void Update()
     {
+        
         // Move Right
         if (Input.GetKey(KeyCode.D))
         {
+            //rotate the particle emitter the opposite direction of the player's movement
             ParticleSystem.ShapeModule shape = particleSystem.shape;
             shape.rotation = new Vector3(329.38f, 270, 0.0f);
+            // add force to player gameobject
             rigidBody.AddForce(Vector2.right * speed * Time.deltaTime, ForceMode2D.Impulse);
+            // emit particles
             doEmitParticles = true;
         }
         else if (!Input.GetKey(KeyCode.A))
         {
+            // if no movement keys are being pressed, dont emit particles
             doEmitParticles = false;
         }
         
@@ -52,20 +62,25 @@ public class Movement : MonoBehaviour
         // Move Left
         if (Input.GetKey(KeyCode.A))
         {
+            //rotate the particle emitter the opposite direction of the player's movement
             ParticleSystem.ShapeModule shape = particleSystem.shape;
             shape.rotation = new Vector3(221.8f, 270, 0.0f);
+            // add force to player gameobject
             rigidBody.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
+            // emit particles
             doEmitParticles = true;
         }
         else if (!Input.GetKey(KeyCode.D))
         {
+            // if no movement keys are being pressed, dont emit particles
             doEmitParticles = false;
         }
         
         // Jump
         if (Input.GetButtonDown("Jump"))
         {
-            if (rigidBody.velocity.y == 0f)
+            // if player's y velocity is about 0, then emit jump particles and add force to gameobject
+            if (Math.Abs(Math.Round(rigidBody.velocity.y)) == 0f)
             {
                 particleSystemJump.Emit(emitParams, 10);
                 rigidBody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
@@ -79,4 +94,5 @@ public class Movement : MonoBehaviour
         }
 
     }
+    
 }
