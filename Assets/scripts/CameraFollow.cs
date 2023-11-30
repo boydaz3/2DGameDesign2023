6640c8de82a39b3b761f4bc431b98704a9303ff8
ariel <149680205+bikeysilly@users.noreply.github.com>
2023-11-30 09:18:26 -0500

    Importing assets from Unity
    Importing game assets from Unity

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // allows us to target a specifc game object to track its transform values 
+    public Transform target;
+
+    // creating a new vector3 to offset the camera from the player position. the last value is the z space where we can control the depth of the camera
+    public Vector3 offset = new Vector3(0, 2, -10);
+
+    // controls how snappy or slow the camera is following the player
+    public float smoothTime = 0.25f;
+
+    Vector3 currentVelocity; 
+    // Update is called once per frame
+    void LateUpdate()
+    {
+       // here we are transforming the position of the camera using the smoothDamp method. 
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref currentVelocity, smoothTime);
+    }
+}
---
+++ Assets/scripts/Platformer Movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ariel Collins
+public class PlatformerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isJumping = false;
+
+    private Rigidbody2D rb;
+
+    public Animator animator;
+
+    float horizontalMovement = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //code for horizontal movement 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed; 
+        Vector2 moveVector = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+        //player is jumping 
+        if(Input.GetButtonDown("Jump") && !isJumping){
+            moveVector.y = jumpForce;
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        rb.velocity = moveVector;
+
+        if(horizontalInput < 0){
+            transform.localScale = new Vector3(1, 1, 1);
+        }   else if(horizontalInput > 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground")){
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+
+
+        }
+    }
+}
