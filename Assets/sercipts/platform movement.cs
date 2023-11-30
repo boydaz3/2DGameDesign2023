5c872ad231ff4a1ae6b1f20e346cdb4815a8566d
Trenten <149679909+YOiBoITobi@users.noreply.github.com>
2023-11-30 08:42:29 -0500

    Transfer files

---
+++ Assets/sercipts/platform movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformmovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isJumping = false;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //code for horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 moveVector = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+//player is jumping
+if(Input.GetButtonDown("Jump") && !isJumping){
+    moveVector.y = jumpForce;
+     isJumping = true;
+    }
+    rb.velocity = moveVector;
+
+    if(horizontalInput < 0){
+        transform.localScale = new Vector3(-1, 1, 1);
+    } else if(horizontalInput > 0){
+        transform.localScale = new Vector3(-1, 1, 1);
+
+    }transform.localScale = new Vector3(-1, 1, 1);
+}
+
+
+private void OnCollisionEnter2D(Collision2D collision){
+    if (collision.gameObject.CompareTag("Ground")){
+    isJumping = false;
+    }
+}
+}
