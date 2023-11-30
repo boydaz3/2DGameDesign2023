5a2110a8d87ba7ca6f757ce62324b385b6f51589
TheKitCatto <149898533+TheKitCatto@users.noreply.github.com>
2023-11-30 08:42:10 -0500

    Adding initial files

---
+++ Assets/Scripts/platfromerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Made by Kieran Smith
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isJumping = false;
+
+    private Rigidbody2D rb;
+
+    public Animator animator;
+
+    float horizontalMovement = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+        Vector2 moveVector = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+        if(Input.GetButtonDown("Jump") && !isJumping){
+            moveVector.y = jumpForce;
+            isJumping = true;
+            animator.SetBool("isJumping" , true);
+        }
+        rb.velocity = moveVector;
+
+        if(horizontalInput < 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if(horizontalInput > 0){
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Ground")){
+            isJumping = false;
+            animator.SetBool("isJumping" , false);
+        }
+    }
+}
