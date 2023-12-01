243a3cc09afd3d94cb71028c703b107736fc1ed6
ChristmasDuck <149620089+ChristmasDuck@users.noreply.github.com>
2023-12-01 13:02:02 -0500

    Added game files

---
+++ Assets/Scripts/prallax.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prallax : MonoBehaviour
+{
+    private float startPos;
+    public GameObject cam;
+    public float prallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (cam.transform.position.x * prallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
