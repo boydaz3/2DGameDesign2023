455d721fbc75b7df59c69fed6882855ea46a6551
Zaya575 <148561637+Zaya575@users.noreply.github.com>
2023-12-01 13:02:00 -0500

    Entire Game Added - 12/1/2023

---
+++ Assets/Camera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+
+    public Vector3 offset = new Vector3(0, 2, -10);
+
+
+    public float smoothTime = 0.25f;
+
+    Vector3 currentVelocity;
+
+    void LateUpdate()
+    {
+
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref currentVelocity, smoothTime);
+    }
+
+}
