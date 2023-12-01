d7bf29f6eef6049de9cfce4491792624fe5fe4b6
ChristmasDuck <149620089+ChristmasDuck@users.noreply.github.com>
2023-12-01 13:22:34 -0500

    Fixed errors and praticed Github saving

--- Assets/Scripts/Enemydestroy.cs
+++ Assets/Scripts/Enemydestroy.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemydestroy : MonoBehaviour
@@ -13,3 +12,4 @@ private void OnTriggerEnter2D(Collider2D collision)
         }
     }
 }
+//this is a comment mehehe
