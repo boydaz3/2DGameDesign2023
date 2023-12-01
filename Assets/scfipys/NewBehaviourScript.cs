aa79f3453367e0c77731edeffa99e07612e5a328
juanbermudez-1 <149680394+juanbermudez-1@users.noreply.github.com>
2023-12-01 08:08:47 -0500

    Initial Project

---
+++ Assets/scfipys/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 5.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+       float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+    }
+}
