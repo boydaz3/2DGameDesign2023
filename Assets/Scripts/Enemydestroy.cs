5ef6a49d728e8eeff921dcbdb21d27da3cdf1cb7
ChristmasDuck <149620089+ChristmasDuck@users.noreply.github.com>
2023-12-04 13:06:56 -0500

    Fixed enemy script

--- Assets/Scripts/Enemydestroy.cs
+++ Assets/Scripts/Enemydestroy.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemydestroy : MonoBehaviour
+    public class Enemydestroy : MonoBehaviour
 {
-private void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
