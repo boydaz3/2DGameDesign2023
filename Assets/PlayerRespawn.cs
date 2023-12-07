a475257cfb7a8d946e79cb7f7ddd68422fc88b7d
Hannah8Bacon <149790835+Hannah8Bacon@users.noreply.github.com>
2023-12-07 07:22:52 -0500

    Background Parallax Effect

---
+++ Assets/PlayerRespawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            if(!IsCollisionFromTop(collision))
+            {
+            RestartLevel();
+            }
+        }
+    }
+
+    private bool IsCollisionFromTop(Collision2D collision)
+    {
+        return transform.position.y > collision.gameObject.transform.position.y;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
