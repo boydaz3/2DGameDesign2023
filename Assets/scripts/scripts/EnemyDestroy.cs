f30338d382705d51a4258884398a691fa3d95ea5
racconWizard <149686668+racconWizard@users.noreply.github.com>
2023-12-08 08:05:11 -0500

    poof gone

--- Assets/scripts/scripts/EnemyDestroy.cs
+++ Assets/scripts/scripts/EnemyDestroy.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 { 
-private void OnTriggerEnter2D(Collider2D collision)
+private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
+            Destroy(transform.parent.gameObject);
         }
     } 
 }
