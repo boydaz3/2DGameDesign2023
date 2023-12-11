0774ec10c0c16290a75445de9e0b1f2aac7dfc6a
EvanSchoolGitHub <149677844+EvanSchoolGitHub@users.noreply.github.com>
2023-12-11 12:16:25 -0500

    Updates made
    Capped velocity, created a death zone, player lives, and start screen.

--- Assets/Scripts/PlatformerMovement.cs
+++ Assets/Scripts/PlatformerMovement.cs
@@ -13,6 +13,8 @@ public class PlatformerMovement : MonoBehaviour
     public Animator animator;
 
     float horizontalMovement = 0f;
+
+    public float maxVelocity = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +44,11 @@ public class PlatformerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1,1,1);
         } else if(horizontalInput > 0){
             transform.localScale = new Vector3(1,1,1);
+        }
+
+        if(rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
         }
     }
     public void OnCollisionEnter2D(Collision2D collision){
--- Assets/Scripts/playerRespawn.cs
+++ Assets/Scripts/playerRespawn.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class playerRespawn : MonoBehaviour
 {
+    public static int playerLives = 3;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Enemy"))
@@ -12,6 +13,21 @@ public class playerRespawn : MonoBehaviour
             if(!IsCollisionFromTop(collision))
             {
                 RestartLevel();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("DeathZone"))
+        {
+            playerLives--;
+            if(playerLives > 0)
+            {
+                Debug.Log(playerLives + "lives left.");
+                RestartLevel();
+            }else
+            {
+                Debug.Log("Game Over");
             }
         }
     }
