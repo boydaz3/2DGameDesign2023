caf6b6f00dafb006b4c3257e43e58f72cb02edfc
Trenten <149679909+YOiBoITobi@users.noreply.github.com>
2023-12-12 08:14:08 -0500

    Added the enemy

--- Assets/sercipts/Paraliax.cs
+++ Assets/sercipts/Paraliax.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Paralix : MonoBehaviour
 {
+    private float startPos;
+    public GameObject cam;
+    public  float prallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = (cam.transform.position.x * prallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
     }
 }
