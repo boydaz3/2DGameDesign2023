aaea9261ca8731dd7bbdc1746ee89553aedaa75c
kone <70107937+konekowo@users.noreply.github.com>
2023-12-12 13:22:06 -0500

    fix enemy kill, added main menu

--- Assets/EnemyDestroy.cs
+++ Assets/EnemyDestroy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
+    public PhysicsMaterial2D enemyHitBoxMaterial;
+    public Collider2D collider;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "EnemySprite")
+        collider = collision.collider;
+        if (collision.gameObject.name == "EnemySprite" && collision.collider.sharedMaterial == enemyHitBoxMaterial)
         {
+            
             Destroy(collision.gameObject);
         }
     }
