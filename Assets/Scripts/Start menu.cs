d0736fd624059bcc5861090282d584946179e4ff
ChristmasDuck <149620089+ChristmasDuck@users.noreply.github.com>
2023-12-12 13:24:15 -0500

    start menu

---
+++ Assets/Scripts/Start menu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startmenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Ma game");
+    }
+}
