fa33dea9d18c730c271a2eb6c01aa4cdf12674a8
ThatOneGus <149606961+ThatOneGus@users.noreply.github.com>
2023-12-13 13:20:03 -0500

    Enemies

--- Assets/Enemies/Enemies.cs
+++ Assets/Enemies/Enemies.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float EnemySpeed = 5.0f;
+    public GameObject[] wayPoints;
+    private int currentWayPoint;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(wayPoints[currentWayPoint].transform.position, transform.position) < .1f){
+            currentWayPoint++;
+            transform.Rotate(0, 180f, 0);
+            if(currentWayPoint >= wayPoints.Length){
+                currentWayPoint = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPoint].transform.position, Time.deltaTime * EnemySpeed);
+
     }
 }
