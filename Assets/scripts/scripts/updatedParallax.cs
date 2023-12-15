4f0d6e14426bc6916338a24c267e6dfe79a1da89
racconWizard <149686668+racconWizard@users.noreply.github.com>
2023-12-15 09:17:38 -0500

    parallax updated and somewhat start screen

--- Assets/scripts/scripts/updatedParallax.cs
+++ Assets/scripts/scripts/updatedParallax.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class updatedParallax : MonoBehaviour
 {
-    private float Length, startPos
+    private float length, startPos;
     public GameObject cam;
     public float parallaxEffect;
     // Start is called before the first frame update
@@ -13,7 +13,7 @@ public class updatedParallax : MonoBehaviour
         //getting the x value of our starting position 
         startPos = transform.position.x;
         //getting the Length of the sprite
-        Length = GetComponent<SpriteRenderer>().bounds.size.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -21,6 +21,17 @@ public class updatedParallax : MonoBehaviour
     {
         //how far we have moved from start point
         float dist = (cam.transform.position.x * parallaxEffect);
-        //move the 
+        //move sprite based on location of camera 
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        //move the background based on how far we have traveled. changing the start position of the background to where we are now
+        //tells us how far we have moved realtive to the camera 
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+
+        if(temp > startPos + length){
+            startPos += length; //startpos = startpos + length
+        }else if(temp < startPos - length){
+            startPos -= length;
+        }
     }
 }
