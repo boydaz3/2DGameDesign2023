bb787aa58131870c2bda2bbbc2cc5023c3f6a66f
Trenten <149679909+YOiBoITobi@users.noreply.github.com>
2023-12-15 08:20:57 -0500

    Made the Enemy animation

--- Assets/sercipts/EnemyDestroy.cs
+++ Assets/sercipts/EnemyDestroy.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
+    Private Animator enemyAnimator;
+
+    void Start()
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        {
+            enemyAnimator = transform.parent.GetComponet<Animator>();
+        }
         if(collision.gameObject.tag == "Player")
         {
-            Destroy(transform.parent.gameObject);
+            enemyAnimator.SetTrigger("Destroy");
+            Invoke("DestoryEnemy", 0.5f);
         }
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(transform.parent.gameObject);
     }
 }
