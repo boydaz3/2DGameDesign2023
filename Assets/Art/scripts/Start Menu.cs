487452041bfc8403dc0c87f0760e5b05b671bb7c
TejiaWilliams <149680379+TejiaWilliams@users.noreply.github.com>
2024-01-11 08:22:48 -0500

    completed start video

---
+++ Assets/Art/scripts/Start Menu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+(
+public void StartGame()
+(
+    SceneManager.LoadScene("Level one");
+)
+)
