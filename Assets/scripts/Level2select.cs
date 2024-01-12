fd0f799be570f2880f9a90489cecdd0dafba30ce
Analiz <144705321+Nakano-11037@users.noreply.github.com>
2024-01-12 09:10:01 -0500

    1/12/24 9:09am
    level 2, level select screen

---
+++ Assets/scripts/Level2select.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level2select : MonoBehaviour
+{
+    public void StartLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
--- Assets/scripts/StartMenu.cs
+++ Assets/scripts/StartMenu.cs
@@ -7,7 +7,16 @@ public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
+        SceneManager.LoadScene("Level Select");
+    }
+
+     public void StartLevel1()
+    {
         SceneManager.LoadScene("Level1");
     }
 
+     public void StartLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
 }
